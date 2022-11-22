dc0fa0088b2def949b329f9fd722e76538cda205
alexhochart <alexhochart@gmail.com>
2022-11-22 14:38:21 +0100

    yy

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const db = require('cyclic-dynamodb')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors());
+app.use(cors({
+  origin:'*'
+}));
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
