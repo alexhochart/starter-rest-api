2ae122c7c45367c34dd742ecff8eeb77ea1de760
alexhochart <alexhochart@gmail.com>
2022-11-22 16:26:31 +0100

    ff

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS '+origin))
+      callback(new Error('Not allowed by CORS origin:'+origin))
     }
   },
   credentials:true,            //access-control-allow-credentials:true
