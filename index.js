1de1b8b3e8fdda5da40593910fa3d74b1f305086
alexhochart <alexhochart@gmail.com>
2022-11-22 08:58:06 +0100

    test

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ app.get('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
+  // const item = await db.collection(col).get(key)
+  // console.log(JSON.stringify(item, null, 2))
+  res.json({test:"alex"}).end()
 })
 
 // Get a full listing
